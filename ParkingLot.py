541bdad352a3d0a11f80fbc11808bf9ec3b19527
PraveenKumarPonraj <praveenkumar@5gindia.net>
2021-04-15 13:48:43 +0530

    assessment 1

--- ParkingLot.py
+++ ParkingLot.py
@@ -1,50 +1,55 @@
 my_list=[]
 vehicle_num=[]
-
+add_list=[10]
 
 def vehicle_add():
-            
-        start=10  
-        end=1
-        if (start>=end):
-            print('\t\t',"---VEHICLE CATEGORY---:")
-            #print("you are in ",i,"floor")
-            print("\n1. Two-wheeler","\n2. Three-wheeler","\n3. Four-","\n4. Others")
-            vehicle_type=int(input("Vehicle type:"))
-            vehicle_number=input("Enter vehicle Number:")
-            vehicle_num.append(vehicle_number)
-            my_list.append(vehicle_type)
-            print('slots occupied:',start,'to',start-vehicle_type+1)
-            print('nextslot',start-vehicle_type)
-            nextslot=start-vehicle_type
-                    
-            if nextslot==0:
-                print("Full,Please go to nextfloor ")
-               
-                main_menu()
+                
+                    slot=add_list[-1]
+                    end=1
+                    if slot >= end :
+
+                        print('\t\t',"---VEHICLE CATEGORY---")
+                        #print("you are in ","floor")
+                        print("\n1. Two-wheeler","\n2. Three-wheeler","\n3. Four-wheeler","\n4. Others" )
+                        vehicle_type=int(input("Vehicle type:"))
+                        vehicle_number=input("Enter vehicle Number:")
+                        vehicle_num.append(vehicle_number)
+                        my_list.append(vehicle_type)
+                        print('slots occupied:',slot,'to',slot-vehicle_type+1)
+                        nextslot=0
+                        nextslot=slot-vehicle_type
+                        print('nextslot is:',nextslot)
+                        add_list.append(nextslot)
+
+                        if nextslot <= 0:
+                            print("Full,Please go to nextfloor ")
+                                            
+                        else:    
+                            print('\nRecord added successfully...')
                         
-            else:    
-                print('\nRecord added successfully...')
-                start=nextslot
-                      
+                    
               
 
 def vehicle_remove():    
     remove=input("Enter the vehicle number:")
-    if not vehicle_num:
-        print("List is Empty")
+    if remove not  in vehicle_num:
+        print("Incorrect vehicle Number")
+        user_key=input("Press any key to exit:")
+        main_menu()
     else:
         vehicle_num.remove(remove)
-        print("vehicle Number",remove,"out from the parking slot successfully")
+        print("vehicle Number",remove,"removed from the parking slot successfully")
 
 
 def view_status():
     print('\t\t',"---VEHICLE IN PARKING SLOT---")
     if not vehicle_num:
         print("List is Empty")
+        user_key=input("Press any key to exit:")
+        main_menu()
     else:
         for view in vehicle_num:
-            print("vehicle number:",view)
+            print("vehicle number:",view )
 
 
 def back():
@@ -55,7 +60,7 @@ def main_menu():
 
     while True:
         print( "W E L C O M E  TO  P A R K I N G   S Y S T E M")
-        print("*************************************************")
+        print("***************************************************")
         print("\n1.IN","\n2.OUT","\n3.View Status","\n4.Exit")
         choice=int(input("Enter your choice :"))
         
@@ -68,7 +73,8 @@ def main_menu():
             view_status()
         if choice==4:
             back() 
-            break
+          
+            
 
 
 if __name__ == "__main__":
