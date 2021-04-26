6f84bffbeb35e24571df984dd8420628bdf8a0bb
PraveenKumarPonraj <praveenkumar@5gindia.net>
2021-04-26 09:46:19 +0530

    Assessment 1

--- ParkingLot.py
+++ ParkingLot.py
@@ -1,44 +1,63 @@
 my_list=[]
 vehicle_num=[]
 add_list=[10]
+view_list=[10]
+my_floor=[4,3,2,1,0]
+floor=[0]
 
 def vehicle_add():
-                
-                    slot=add_list[-1]
-                    end=1
-                    if slot >= end :
+        
+    slot=add_list[-1]
+    last_slot=0
+    if slot >= last_slot :
+        i=0
+        i=floor[-1]
+        print('\t\t',"---VEHICLE CATEGORY---")
+        print("you are in ",my_floor[i],"floor")
+        print("\n1. Two-Wheeler","\n2. Three-Wheeler","\n3. Four-Wheeler","\n4. Others" )
+        vehicle_type=int(input("Vehicle type:"))
+        if vehicle_type > 4:
+            print("Incorrect")
+        else:
+            vehicle_number=input("Enter Vehicle Number:")
+            vehicle_num.append(vehicle_number)
+            my_list.append(vehicle_type)
+            print('\nSlots Occupied:',slot,'to',slot-vehicle_type+1)
+            if slot-vehicle_type < 0:
+                print("\nInvalid,Please select within the slot") 
+            else:    
+                view_list.append(slot)
+                nextslot=0
+                nextslot=slot-vehicle_type
+                try:
+                    print('Available Slots in this Floor:',nextslot)
+                    add_list.append(nextslot)
+                    assert nextslot > 0
+                except AssertionError:
+                    pass
 
-                        print('\t\t',"---VEHICLE CATEGORY---")
-                        #print("you are in ","floor")
-                        print("\n1. Two-wheeler","\n2. Three-wheeler","\n3. Four-wheeler","\n4. Others" )
-                        vehicle_type=int(input("Vehicle type:"))
-                        vehicle_number=input("Enter vehicle Number:")
-                        vehicle_num.append(vehicle_number)
-                        my_list.append(vehicle_type)
-                        print('slots occupied:',slot,'to',slot-vehicle_type+1)
-                        nextslot=0
-                        nextslot=slot-vehicle_type
-                        print('nextslot is:',nextslot)
-                        add_list.append(nextslot)
-
-                        if nextslot <= 0:
-                            print("Full,Please go to nextfloor ")
-                                            
-                        else:    
-                            print('\nRecord added successfully...')
-                        
-                    
-              
+            
+                if nextslot <= 0:
+                    i=i+1
+                    floor.append(i)
+                    print("Full,Please go to nextfloor ")
+                    add_list.append(10)
+                    main_menu()
+                                                    
+                else:    
+                    print('\nRecord added successfully...')
+                            
 
 def vehicle_remove():    
     remove=input("Enter the vehicle number:")
-    if remove not  in vehicle_num:
+    if remove not in vehicle_num:
         print("Incorrect vehicle Number")
         user_key=input("Press any key to exit:")
         main_menu()
     else:
         vehicle_num.remove(remove)
         print("vehicle Number",remove,"removed from the parking slot successfully")
+        
 
 
 def view_status():
@@ -48,9 +67,11 @@ def view_status():
         user_key=input("Press any key to exit:")
         main_menu()
     else:
-        for view in vehicle_num:
-            print("vehicle number:",view )
-
+        #for view in vehicle_num:
+            print("vehicle number:",vehicle_num[-1])
+            slot=view_list[-1]
+            print("IN slot",slot)
+                
 
 def back():
     pass
@@ -60,11 +81,9 @@ def main_menu():
 
     while True:
         print( "W E L C O M E  TO  P A R K I N G   S Y S T E M")
-        print("***************************************************")
-        print("\n1.IN","\n2.OUT","\n3.View Status","\n4.Exit")
+        print("*************************************************")
+        print("\n1.IN","\n2.OUT","\n3.VIEW STATUS","\n4.EXIT")
         choice=int(input("Enter your choice :"))
-        
-
         if choice==1:
             vehicle_add()
         if choice==2: 
@@ -73,9 +92,8 @@ def main_menu():
             view_status()
         if choice==4:
             back() 
-          
-            
-
+        if choice > 4:
+            print("Invalid!") 
 
 if __name__ == "__main__":
     main_menu()            
